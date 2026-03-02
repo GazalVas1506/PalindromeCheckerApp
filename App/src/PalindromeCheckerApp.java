908b3e3b274649810a063849ce9dde9f1f9407e6
Karthikeyan_S <maxrest77@gmail.com>
2026-03-02 10:34:18 +0530

    UC1: Application Entry & Welcome Message

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("=== Palindrome Checker Application ===");
+        System.out.println("Version: 1.0.0");
+        System.out.println("Application started successfully.");
+    }
+}
